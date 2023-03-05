a36fb8d431e275b19497093a2ef9d90c85d61554
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-03-05 01:35:52 -0300

    feat: cadastrando e listando os dados

--- crud-api/src/app/components/people/people.component.ts
+++ crud-api/src/app/components/people/people.component.ts
@@ -1,5 +1,7 @@
+import { PeopleService } from './../../services/people.service';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Person } from 'src/app/person/person';
 
 @Component({
   selector: 'app-people',
@@ -10,10 +12,23 @@ export class PeopleComponent implements OnInit {
 
   form: any;
   titleForm?: string;
+  people?: Person[];
+  tableVisibility?: boolean = true;
+  formVisibility?: boolean = false;
 
-  constructor() { }
+  constructor(private peopleService: PeopleService) { }
 
   ngOnInit(): void {
+    this.peopleService.getAll().subscribe(result => {
+      this.people = result;
+    });
+
+  }
+
+  showForm(): void {
+    this.tableVisibility = false;
+    this.formVisibility = true;
+    this.titleForm = 'Nova Pessoa',
     this.form = new FormGroup({
       name: new FormControl(null),
       lastName: new FormControl(null),
@@ -22,6 +37,24 @@ export class PeopleComponent implements OnInit {
       address: new FormControl(null),
       profession: new FormControl(null),
     });
+  }
+
+  sendForm(): void {
+    const person: Person = this.form.value;
+
+    this.peopleService.savePerson(person).subscribe((result) => {
+      this.formVisibility = false;
+      this.tableVisibility = true;
+      alert('Pessoa cadastrada com sucesso');
+      this.peopleService.getAll().subscribe(result => {
+        this.people = result;
+      });
+    });
+  }
+
+  goToBack(): void {
+    this.tableVisibility = true;
+    this.formVisibility = false;
   }
 
 }
